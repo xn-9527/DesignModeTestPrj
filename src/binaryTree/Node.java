36fd500bd2f1a91b87b5776c64e89b6bccc41bfb
chay.ni <chay.ni@mrobot.cn>
2017-12-19 14:00:18 +0800

    测试二叉树

---
+++ src/binaryTree/Node.java
@@ -0,0 +1,21 @@
+package binaryTree;
+
+/**
+ * Created by xiaoni on 2017/10/18.
+ */
+class Node
+{
+    public int iData;              // data item (key)
+    public double dData;           // data item
+    public Node leftChild;         // this node's left child
+    public Node rightChild;        // this node's right child
+
+    public void displayNode()      // display ourself
+    {
+        System.out.print('{');
+        System.out.print(iData);
+        System.out.print(", ");
+        System.out.print(dData);
+        System.out.print("} ");
+    }
+}  // end class Node
