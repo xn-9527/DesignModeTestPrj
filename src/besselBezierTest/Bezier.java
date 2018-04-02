79be88ec29b45ceda36d02b07c2e0db71ce6f2d4
chay.ni <chay.ni@mrobot.cn>
2018-04-02 11:31:15 +0800

    扩展n阶贝塞尔曲线

--- src/besselBezierTest/Bezier.java
+++ src/besselBezierTest/Bezier.java
@@ -16,17 +16,30 @@ import java.awt.geom.Point2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class test2 extends JPanel implements MouseListener,MouseMotionListener{
+public class Bezier extends JPanel implements MouseListener,MouseMotionListener{
     private static final long serialVersionUID = 1L;
     private Point2D[] controlPoint;
     private Ellipse2D.Double[] ellipse;
     private static final double SIDELENGTH = 8;
+    //当前画的控制点的第几个点
     private int numPoints;
+    //几阶贝塞尔(等于起点、终点、控制点数量的总和)
+    private int orderNum;
+    //画点的分辨率
+//    private double t=0.002;
     private double t=0.002;
-    public test2(){
+    public Bezier(){
         numPoints = 0;
         controlPoint = new Point2D[4];
         ellipse = new Ellipse2D.Double[4];
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+    }
+    public Bezier(int orderNum){
+        numPoints = 0;
+        this.orderNum = orderNum;
+        controlPoint = new Point2D[orderNum];
+        ellipse = new Ellipse2D.Double[orderNum];
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
     }
@@ -42,10 +55,11 @@ public class test2 extends JPanel implements MouseListener,MouseMotionListener{
             }else {
                 g2.draw(ellipse[i]);
             }
-            if(numPoints>1 && i<(numPoints - 1))
-                g2.drawLine((int)controlPoint[i].getX(), (int)controlPoint[i].getY(), (int)controlPoint[i+1].getX(), (int)controlPoint[i+1].getY());
+            if(numPoints>1 && i<(numPoints - 1)) {
+                g2.drawLine((int) controlPoint[i].getX(), (int) controlPoint[i].getY(), (int) controlPoint[i + 1].getX(), (int) controlPoint[i + 1].getY());
+            }
         }
-        if(numPoints == 4) {
+        /*if(numPoints == 4) {
             double x,y;
             g2.setColor(Color.RED);
             for(double k=t; k<=1+t; k+=t) {
@@ -56,8 +70,55 @@ public class test2 extends JPanel implements MouseListener,MouseMotionListener{
 //              System.out.println(x + "  " + y );
                 g2.drawLine((int)x, (int)y, (int)x, (int)y);
             }
+        }*/
+        if(numPoints == orderNum) {
+            double x,y;
+            g2.setColor(Color.RED);
+            for(double k=t; k<=1+t; k+=t) {
+                CvPoint2D cvPoint2D = calBezierPoint(k, controlPoint);
+                x = cvPoint2D.getX();
+                y = cvPoint2D.getY();
+                /*x = (1-k)*(1-k)*(1-k)*controlPoint[0].getX() + 3*k*(1-k)*(1-k)*controlPoint[1].getX()
+                        + 3*k*k*(1-k)*controlPoint[2].getX() + k*k*k*controlPoint[3].getX();
+                y = (1-k)*(1-k)*(1-k)*controlPoint[0].getY() + 3*k*(1-k)*(1-k)*controlPoint[1].getY()
+                        + 3*k*k*(1-k)*controlPoint[2].getY() + k*k*k*controlPoint[3].getY();*/
+              System.out.println(x + "  " + y );
+              g2.drawLine((int)x, (int)y, (int)x, (int)y);
+            }
         }
     }
+
+    /**
+     * 根据控制点序列，计算n阶贝塞尔曲线上的一个点
+     * @param u 步长
+     * @param controlPoint 控制点序列，含起点、终点
+     * @return
+     */
+    public CvPoint2D calBezierPoint(double u,Point2D[] controlPoint) {
+        if(controlPoint == null || controlPoint.length <= 0) {
+            System.out.println("控制点列表为空");
+            return null;
+        }
+        int orderNum = controlPoint.length;
+        CvPoint2D[] p = new CvPoint2D[orderNum];
+        for (int i = 0; i < orderNum; i++) {
+            if(controlPoint[i] == null) {
+                System.out.println("控制点中有空对象，说明用户还没建立足够的点");
+                return null;
+            }
+
+            p[i] = new CvPoint2D(controlPoint[i].getX(), controlPoint[i].getY());
+        }
+
+        for (int r = 1; r < orderNum; r++) {
+            for (int i = 0; i < orderNum - r; i++) {
+                p[i].setX((1 - u) * p[i].getX() + u * p[i + 1].getX());
+                p[i].setY((1 - u) * p[i].getY() + u * p[i + 1].getY());
+            }
+        }
+        return p[0];
+    }
+
     @Override
     public Dimension getPreferredSize() {
         // TODO Auto-generated method stub
@@ -66,7 +127,7 @@ public class test2 extends JPanel implements MouseListener,MouseMotionListener{
 
     public static void main(String[] agrs) {
         JFrame f = new JFrame();
-        test2 t2 = new test2();
+        Bezier t2 = new Bezier(5);
         f.add(t2);
         f.pack();
         f.setVisible(true);
@@ -75,7 +136,8 @@ public class test2 extends JPanel implements MouseListener,MouseMotionListener{
     @Override
     public void mouseClicked(MouseEvent e) {
         // TODO Auto-generated method stub
-        if(numPoints <4) {
+//        if(numPoints <4) {
+        if(numPoints < orderNum) {
             double x = e.getX();
             double y = e.getY();
             controlPoint[numPoints] = new Point2D.Double(x, y);
@@ -91,7 +153,8 @@ public class test2 extends JPanel implements MouseListener,MouseMotionListener{
         // TODO Auto-generated method stub
     }
     private int find(Point2D p){
-        int flag = 5;
+//        int flag = 5;
+        int flag = orderNum + 1;
         for(int i=0; i<numPoints; i++) {
             if( ellipse[i].contains(p)) {
                 flag = i;
@@ -116,7 +179,8 @@ public class test2 extends JPanel implements MouseListener,MouseMotionListener{
     public void mouseDragged(MouseEvent e) {
         // TODO Auto-generated method stub
         int flag  = find((Point2D)e.getPoint());
-        if( flag<5) {
+//        if( flag<5) {
+        if( flag<orderNum + 1) {
             double x = e.getX();
             double y = e.getY();
             controlPoint[flag] = new Point2D.Double(x, y);
