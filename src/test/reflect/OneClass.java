ec377c54b261314116934560f18565243aded05f
chay.ni <chay.ni@mrobot.cn>
2018-09-08 19:05:00 +0800

    测试反射调用类的方法、获取属性类型、值。

--- src/test/reflect/OneClass.java
+++ src/test/reflect/OneClass.java
@@ -4,4 +4,39 @@ package test.reflect;
  * Created by xiaoni on 2018/9/8.
  */
 public class OneClass {
+
+    String name;
+    Integer age;
+
+    public void printInfo(){
+        System.out.println("打印学生信息");
+    }
+
+    public void printAddress(String address){
+        System.out.println("hellow,I come from "+address);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "OneClass{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
