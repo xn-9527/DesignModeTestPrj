d744a9f38a2d788ff91b7f9535d531c566f80725
chay.ni <chay.ni@mrobot.cn>
2018-10-17 20:40:04 +0800

    测试@schedule定时任务

--- src/main/java/cn/quartz/QuartzTest.java
+++ src/main/java/cn/quartz/QuartzTest.java
@@ -1,12 +1,32 @@
 package cn.quartz;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * @author Created by xiaoni on 2018/10/17.
  */
+@Configuration
+@EnableScheduling  //启用定时任务
 public class QuartzTest {
     /**
+     * 基本配置定时任务时间的格式就是“秒  分  时 月 日 星期几 年”，不想设置的字段就? 。
+     附录：
+     cronExpression的配置说明，具体使用以及参数请百度google
+     字段   允许值   允许的特殊字符
+     秒    0-59    , - * /
+     分    0-59    , - * /
+     小时    0-23    , - * /
+     日期    1-31    , - * ? / L W C
+     月份    1-12 或者 JAN-DEC    , - * /
+     星期    1-7 或者 SUN-SAT    , - * ? / L C #
+     年（可选）    留空, 1970-2099    , - * /
+     - 区间
+     * 通配符
+     ? 你不想设置那个字段
+     下面只例出几个式子
+
      * CRON表达式    含义
      "0 0 12 * * ?"    每天中午十二点触发
      "0 15 10 ? * *"    每天早上10：15触发
@@ -22,7 +42,7 @@ public class QuartzTest {
      0 0 *(去掉这个)/1 * * ?    每天隔一小时
      0 0 1-8/1 * * ?    从凌晨一点到早上八点每隔一小时
      */
-    @Scheduled(cron = "0 0 * * */1 ?")
+    @Scheduled(cron = "*/1 * * * * ?")
     public void test1() {
         System.out.println("我是test1");
     }
