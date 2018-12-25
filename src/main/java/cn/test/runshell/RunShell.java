e2a6eb150a661f9f3b951eef94c292208596f13a
chay.ni <chay.ni@mrobot.cn>
2018-12-25 11:11:53 +0800

    test java run shell

--- src/main/java/cn/test/runshell/RunShell.java
+++ src/main/java/cn/test/runshell/RunShell.java
@@ -1,39 +1,47 @@
-//package cn.test.runshell;
-//
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//
-///**
-// * @author Created by xiaoni on 2018/12/24.
-// */
-//public class RunShell {
-//    public static void main(String[] args) {
-//        try {
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static void runShell() {
-//        //            String shpath="G:\\WorkSpaceSSD\\DesignModeTestPrj\\src\\main\\java\\cn\\test\\runshell\\demo-classes.sh";
-//        String shpath = "G:\\WorkSpaceSSD\\DesignModeTestPrj\\src\\main\\java\\cn\\test\\runshell\\demo-classes.bat";
-//        try (Process ps = Runtime.getRuntime().exec(shpath);) {
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } ;
-//        ps.waitFor();
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
-//        StringBuffer sb = new StringBuffer();
-//        String line;
-//        while ((line = br.readLine()) != null) {
-//            sb.append(line).append("\n");
-//        }
-//        String result = sb.toString();
-//        System.out.println(result);
-//    }
-//}
+package cn.test.runshell;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Created by xiaoni on 2018/12/24.
+ */
+public class RunShell {
+    public static void main(String[] args) {
+        try {
+            runShell();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void runShell() {
+        //            String shpath="G:\\WorkSpaceSSD\\DesignModeTestPrj\\src\\main\\java\\cn\\test\\runshell\\demo-classes.sh";
+        Process ps = null;
+        try {
+            String shpath = "G:\\WorkSpaceSSD\\DesignModeTestPrj\\src\\main\\java\\cn\\test\\runshell\\demo-classes.bat";
+            ps = Runtime.getRuntime().exec(shpath);
+            ps.waitFor();
+
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));) {
+                StringBuffer sb = new StringBuffer();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                String result = sb.toString();
+                System.out.println(result);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (ps != null) {
+                ps.destroy();
+            }
+        }
+    }
+}
