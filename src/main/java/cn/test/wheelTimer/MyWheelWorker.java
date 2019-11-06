3e366be22cc7fbc6bf12346a16064235a609e03e
chay.ni <chay.ni@mrobot.cn>
2019-11-06 11:45:43 +0800

    test myWheelTimer

--- src/main/java/cn/test/wheelTimer/MyWheelWorker.java
+++ src/main/java/cn/test/wheelTimer/MyWheelWorker.java
@@ -13,6 +13,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class MyWheelWorker implements Runnable, Comparable<MyWheelWorker> {
     /**
+     * 最小的触发毫秒数
+     */
+    private final static long MIN_TRIGGER_MILLIS = 2L;
+    /**
+     * 睡眠预留处理时间
+     */
+    private final static long SLEEP_PRE_TRIGGER_MILLIS = 3L;
+
+    /**
      * 时间轮工人id，整个系统全局唯一
      */
     private static final AtomicInteger AUTO_ID = new AtomicInteger();
@@ -31,12 +40,28 @@ public class MyWheelWorker implements Runnable, Comparable<MyWheelWorker> {
      */
     private long roundCount;
     private Runnable runnable;
+    /**
+     * 当任务中止时，需要显示的消息
+     */
+    private String interruptMessage;
 
     public MyWheelWorker(long triggerTime, Runnable runnable) {
+        if (triggerTime <= 0) {
+            throw new IllegalArgumentException("triggerTime must bigger than 0:" + triggerTime);
+        }
+        if (runnable == null) {
+            throw new NullPointerException("runnable is null.");
+        }
+
         AUTO_ID.incrementAndGet();
         this.id = AUTO_ID.get();
         this.triggerTime = triggerTime;
         this.runnable = runnable;
+    }
+
+    public MyWheelWorker(long triggerTime, Runnable runnable, String interruptMessage) {
+        this(triggerTime, runnable);
+        this.interruptMessage = interruptMessage;
     }
 
     @Override
@@ -47,18 +72,18 @@ public class MyWheelWorker implements Runnable, Comparable<MyWheelWorker> {
         //try catch 一定要在while循环外，否则线程无法被打断停止
         try {
             //以2ms为最低触发时限，阻塞线程直到触发时间，再触发任务
-            while (timeUntilTrigger > 2) {
+            while (timeUntilTrigger > MIN_TRIGGER_MILLIS) {
                 now = System.currentTimeMillis();
                 timeUntilTrigger = triggerTime - now;
 
-                if (timeUntilTrigger > 6) {
-                    Thread.sleep(timeUntilTrigger - 3);
+                if (timeUntilTrigger > 2 * SLEEP_PRE_TRIGGER_MILLIS) {
+                    Thread.sleep(timeUntilTrigger - SLEEP_PRE_TRIGGER_MILLIS);
                 }
             }
             log.debug("trigger time : {}", System.currentTimeMillis());
             runnable.run();
         } catch (InterruptedException e) {
-            log.error("worker {} interrupted, work {} will not be done", id, JSON.toJSONString(runnable));
+            log.error("worker {} interrupted, work will not be done. {}", id, interruptMessage);
             log.error("worker " + id + " interrupted " + e.getMessage(), e);
             Thread.currentThread().interrupt();
         }
