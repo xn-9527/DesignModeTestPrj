3370c042457128106df4d7b256f85b8deb2b39fd
chay.ni <chay.ni@mrobot.cn>
2019-11-14 15:56:20 +0800

    test eureka client排除jersey，而使用默认的restTemplate去连接eureka server。以便更好的支持docker client

--- spring-boot-movie-test/src/main/java/cn/chay/ProviderMovieApplication.java
+++ spring-boot-movie-test/src/main/java/cn/chay/ProviderMovieApplication.java
@@ -1,8 +1,8 @@
 package cn.chay;
 
-import com.netflix.discovery.DiscoveryClient;
-import com.sun.jersey.api.client.filter.ClientFilter;
-import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+//import com.netflix.discovery.DiscoveryClient;
+//import com.sun.jersey.api.client.filter.ClientFilter;
+//import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,14 +21,18 @@ public class ProviderMovieApplication {
         return new RestTemplate();
     }
 
-    @Bean
+    //这个ClientFilter配置跟排除pom中springCloud中jersey冲突
+    /**
+     * 用自定义的ClientFilter去设置带权限的访问eureka server
+     */
+    /*@Bean
     public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() {
         DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs = new DiscoveryClient.DiscoveryClientOptionalArgs();
         List<ClientFilter> additionalFilters = new ArrayList<>();
         additionalFilters.add(new HTTPBasicAuthFilter("user", "password123"));
         discoveryClientOptionalArgs.setAdditionalFilters(additionalFilters);
         return discoveryClientOptionalArgs;
-    }
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderMovieApplication.class, args);
