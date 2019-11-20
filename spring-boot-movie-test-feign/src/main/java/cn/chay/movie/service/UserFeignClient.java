070eb1ae34d954dbfcd16f9d42314d2b61d808df
chay.ni <chay.ni@mrobot.cn>
2019-11-20 09:15:53 +0800

    test feign

---
+++ spring-boot-movie-test-feign/src/main/java/cn/chay/movie/service/UserFeignClient.java
@@ -0,0 +1,17 @@
+package cn.chay.movie.service;
+
+import cn.chay.movie.vo.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by xiaoni on 2019/11/20.
+ */
+//name指定ribbon client负载均衡
+@FeignClient(name = "microservice-user")
+public interface UserFeignClient {
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public User findById(@PathVariable("id") Long id);
+}
