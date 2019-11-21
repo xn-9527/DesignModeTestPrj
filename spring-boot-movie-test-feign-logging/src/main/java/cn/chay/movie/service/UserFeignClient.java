11c39be545747bbe422e764f17b68fb7159edb38
chay.ni <chay.ni@mrobot.cn>
2019-11-21 09:52:02 +0800

    test feign logging by yml

--- spring-boot-movie-test-feign-logging/src/main/java/cn/chay/movie/service/UserFeignClient.java
+++ spring-boot-movie-test-feign-logging/src/main/java/cn/chay/movie/service/UserFeignClient.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by xiaoni on 2019/11/20.
  */
-//name指定ribbon client负载均衡,为接口指定日志级别配置类
-@FeignClient(name = "microservice-user", configuration = FeignLogConfiguration.class)
+//name指定ribbon client负载均衡,为接口指定日志级别配置类或者通过yml配置
+//@FeignClient(name = "microservice-user", configuration = FeignLogConfiguration.class)
+@FeignClient(name = "microservice-user")
 public interface UserFeignClient {
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
     public User findById(@PathVariable("id") Long id);
