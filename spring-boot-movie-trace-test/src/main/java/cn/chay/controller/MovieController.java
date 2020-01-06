23bd73e33bfd3f791be2a8d651f6f280c569e0f2
chay.ni <chay.ni@mrobot.cn>
2020-01-06 16:54:57 +0800

    test spring cloud sleuth user and movie.微服务跟踪

--- spring-boot-movie-trace-test/src/main/java/cn/chay/controller/MovieController.java
+++ spring-boot-movie-trace-test/src/main/java/cn/chay/controller/MovieController.java
@@ -1,6 +1,6 @@
 package cn.chay.controller;
 
-import cn.chay.movie.vo.User;
+import cn.chay.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
