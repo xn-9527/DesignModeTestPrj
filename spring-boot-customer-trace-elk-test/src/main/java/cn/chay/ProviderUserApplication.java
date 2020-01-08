537f95082f5b8295c40a2aa1bba40e6d0d65c374
chay.ni <chay.ni@mrobot.cn>
2020-01-08 14:08:09 +0800

    test spring cloud sleuth with ELK(Elasticsearch,Logstash,Kibana)

---
+++ spring-boot-customer-trace-elk-test/src/main/java/cn/chay/ProviderUserApplication.java
@@ -0,0 +1,14 @@
+package cn.chay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Created by xiaoni on 2019/11/12.
+ */
+@SpringBootApplication
+public class ProviderUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderUserApplication.class, args);
+    }
+}
