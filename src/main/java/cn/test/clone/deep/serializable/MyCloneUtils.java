df06a73478090c2559556efcd5d3b397d12bdc30
chay.ni <chay.ni@mrobot.cn>
2020-03-03 22:41:33 +0800

    test deepClone by JSON

--- src/main/java/cn/test/clone/deep/serializable/MyCloneUtils.java
+++ src/main/java/cn/test/clone/deep/serializable/MyCloneUtils.java
@@ -1,5 +1,6 @@
 package cn.test.clone.deep.serializable;
 
+import cn.test.equals.User;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import lombok.extern.slf4j.Slf4j;
@@ -45,6 +46,10 @@ public class MyCloneUtils {
             }
         }
         return clonedObj;
+    }
+
+    public static <T> T deepClone2(T obj) {
+        return JSON.parseObject(JSON.toJSONString(obj), (Class<T>) obj.getClass());
     }
 
     /**
@@ -220,6 +225,12 @@ public class MyCloneUtils {
         } catch (Exception e1) {
             log.error(e1.getMessage(), e1);
         }
+
+        System.out.println("################################");
+        User user = new User("test", "mail", "123");
+        User userClone = MyCloneUtils.deepClone2(user);
+        log.info(JSON.toJSONString(userClone));
+        log.info(String.valueOf(user == userClone));
     }
 
     public static void printMapValueSet(Set set) {
