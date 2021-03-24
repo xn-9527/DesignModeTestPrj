0db0e0d7bc187e1e03b6dbdbec01859a27da078a
nichengyun <nichengyun@souche.com>
2021-03-24 15:19:02 +0800

    test mybatis generator mapper.xml,dao,models

---
+++ mybatis-generator/src/test/java/com/chay/mybatisgenerator/MybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chay.mybatisgenerator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratorApplicationTests {
+
+    @Test
+	void contextLoads() {
+	}
+
+}
