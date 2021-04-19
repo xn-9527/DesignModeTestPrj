99dcef5fc855bdbf01e0136ae405d564400cf9f3
nichengyun <nichengyun@souche.com>
2021-04-19 17:03:02 +0800

    test uuid

---
+++ src/main/java/cn/test/uuid/UUIDTest.java
@@ -0,0 +1,14 @@
+package cn.test.uuid;
+
+import java.util.UUID;
+
+/**
+ * @author: nichengyun
+ * @date: 2021/4/19 14:54 
+ * @description:
+ */
+public class UUIDTest {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID());
+    }
+}
