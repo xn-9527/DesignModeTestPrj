9b47692c0b6a2a77fe0152b4937f5e91c684d6b6
nichengyun <nichengyun@souche.com>
2021-04-27 18:29:50 +0800

    test comparator

--- src/main/java/cn/test/comparator/TestComparator.java
+++ src/main/java/cn/test/comparator/TestComparator.java
@@ -26,14 +26,13 @@ public class TestComparator {
         l.sort(new Comparator<TestComparator>() {
             @Override
             public int compare(TestComparator o1, TestComparator o2) {
-                if (o2 != null && o2.getOrder() != null
-                        && (o1 == null || o1.getOrder() == null)) {
+                boolean isO1OrderNull = o1 == null || o1.getOrder() == null;
+                boolean isO2OrderNull = o2 == null || o2.getOrder() == null;
+                if (!isO2OrderNull && isO1OrderNull) {
                     return -1;
-                } else if (o1 != null && o1.getOrder() != null
-                        && (o2 == null || o2.getOrder() == null)) {
+                } else if (isO2OrderNull && !isO1OrderNull) {
                     return 1;
-                } else if ((o1 == null || o1.getOrder() == null)
-                        && (o2 == null || o2.getOrder() == null)) {
+                } else if (isO2OrderNull && isO1OrderNull) {
                     return 0;
                 } else {
                     return o1.getOrder().compareTo(o2.getOrder());
@@ -43,14 +42,13 @@ public class TestComparator {
         System.out.println(l);
         //从大到小
         l.sort((o1, o2) -> {
-            if (o2 != null && o2.getOrder() != null
-                    && (o1 == null || o1.getOrder() == null)) {
+            boolean isO1OrderNull = o1 == null || o1.getOrder() == null;
+            boolean isO2OrderNull = o2 == null || o2.getOrder() == null;
+            if (!isO2OrderNull && isO1OrderNull) {
                 return 1;
-            } else if (o1 != null && o1.getOrder() != null
-                    && (o2 == null || o2.getOrder() == null)) {
+            } else if (isO2OrderNull && !isO1OrderNull) {
                 return -1;
-            } else if ((o1 == null || o1.getOrder() == null)
-                    && (o2 == null || o2.getOrder() == null)) {
+            } else if (isO2OrderNull && isO1OrderNull) {
                 return 0;
             } else {
                 return o2.getOrder().compareTo(o1.getOrder());
