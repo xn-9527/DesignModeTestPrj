c36623dce80a352b6198e74f9865db3f18925290
chay.ni <chay.ni@mrobot.cn>
2021-05-24 21:20:50 +0800

    uuid test

--- src/main/java/cn/test/uuid/UUIDTest.java
+++ src/main/java/cn/test/uuid/UUIDTest.java
@@ -9,6 +9,6 @@ import java.util.UUID;
  */
 public class UUIDTest {
     public static void main(String[] args) {
-        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
     }
 }
