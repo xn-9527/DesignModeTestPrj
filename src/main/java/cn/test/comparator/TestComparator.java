7e5d504a2c9b599d88bd412d5d5f982cded25504
blackknight <blackknight.ni@tuya.com>
2022-10-28 18:39:04 +0800

    test comparable comparator

--- src/main/java/cn/test/comparator/TestComparator.java
+++ src/main/java/cn/test/comparator/TestComparator.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -55,5 +56,31 @@ public class TestComparator {
             }
         });
         System.out.println(l);
+
+        List<Long> longs = Arrays.asList(3L, 2L, 5L);
+        longs.sort(new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                boolean isO1OrderNull = o1 == null;
+                boolean isO2OrderNull = o2 == null;
+                if (!isO2OrderNull && isO1OrderNull) {
+                    return -1;
+                } else if (isO2OrderNull && !isO1OrderNull) {
+                    return 1;
+                } else if (isO2OrderNull && isO1OrderNull) {
+                    return 0;
+                } else {
+                    if (o1.getClass() != o2.getClass()) {
+                        return 0;
+                    }
+                    //实现了comparable的接口直接调用方法
+                    if (o1 instanceof Comparable) {
+                        return ((Comparable)o1).compareTo((Comparable)o2);
+                    }
+                    return 0;
+                }
+            }
+        });
+        System.out.println(longs);
     }
 }
